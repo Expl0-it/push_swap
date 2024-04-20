1d43ef963b3b6d6c4df70a5f54f69a53f2ccfe19
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-04-20 22:30:38 +0200

    added declarations in header

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 17:51:07 by mamichal          #+#    #+#             */
-/*   Updated: 2024/04/20 18:05:21 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/04/20 22:16:35 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@
 
 enum	e_errors
 {
-	NOT_ENOUGH_ARGUMENTS = -1
+	NOT_ENOUGH_ARGUMENTS = -1,
+	SYNTAX_ERROR = -2,
+	INT_OVERFLOW = -3,
+	NUMBER_REPETITION = -4
 };
 
 typedef struct s_stack_node
@@ -37,6 +40,9 @@ void	stack_init(t_stack_node **a, char **argv, bool is_argc_2);
 
 // error_free.c
 bool	error_syntax(char *str_nb);
+bool	error_repetition(t_stack_node *a, int nbr);
+void	free_stack(t_stack_node **stack);
 void	free_split(char **argv);
+void	error_free(t_stack_node **a, char **argv, bool is_argc_2, int code);
 
 #endif
