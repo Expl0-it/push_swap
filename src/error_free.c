db11572757c629a73acb3e623758956b5fba0920
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-04-20 22:29:57 +0200

    added func for errors and to free in neccesary cases

--- src/error_free.c
+++ src/error_free.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 17:32:09 by mamichal          #+#    #+#             */
-/*   Updated: 2024/04/20 18:07:09 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/04/20 22:14:33 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,38 @@ bool	error_syntax(char *str_nb)
 	return (false);
 }
 
+bool	error_repetition(t_stack_node *a, int nbr)
+{
+	if (NULL == a)
+		return (false);
+	while (NULL != a)
+	{
+		if (a->value == nbr)
+			return (true);
+		a = a->next;
+	}
+	return (false);
+}
+
+void	free_stack(t_stack_node **stack)
+{
+	t_stack_node	*tmp;
+	t_stack_node	*current;
+
+	if (NULL == stack)
+		return ;
+	current = *stack;
+	while (current)
+	{
+		tmp = current->next;
+		free(current);
+		current = tmp;
+	}
+	current = NULL;
+}
+
+
+
 void	free_split(char **argv)
 {
 	int	i;
@@ -42,4 +74,14 @@ void	free_split(char **argv)
 		i++;
 	}
 	free(argv[i]);
+}
+
+void	error_free(t_stack_node **a, char **argv, bool is_argc_2, int code)
+{
+	if (NULL != a)
+		free_stack(a);
+	if (is_argc_2)
+		free_split(argv);
+	write(2, "Error\n", 6);
+	exit(code);
 }
