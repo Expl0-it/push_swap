95205fa2ea7d5b57bb51fda42be12a3cddca0e5f
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-04-21 21:24:59 +0200

    changed variable types to match new list name, changed split to use argv[1]

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 17:50:45 by mamichal          #+#    #+#             */
-/*   Updated: 2024/04/20 17:25:32 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/04/21 20:58:17 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	main(int argc, char **argv)
 {
-	t_stack_node	*a;
-	t_stack_node	*b;
+	t_db_list	*a;
+	t_db_list	*b;
 
 	a = NULL;
 	b = NULL;
@@ -23,6 +23,6 @@ int	main(int argc, char **argv)
 	if (argc < 2 || !argv[1][0])
 		return (NOT_ENOUGH_ARGUMENTS);
 	else if (2 == argc)
-		argv = ft_split(*argv, ' ');
+		argv = ft_split(argv[1], ' ');
 	stack_init(&a, argv, 2 == argc);
 }
