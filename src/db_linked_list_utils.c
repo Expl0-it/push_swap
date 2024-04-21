035259aabd70f7ded78e9fb7b72d5571f46d9bce
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-04-21 16:01:10 +0200

    added functions to work with double linked list

--- src/db_linked_list_utils.c
+++ src/db_linked_list_utils.c
@@ -6,21 +6,53 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 22:22:52 by mamichal          #+#    #+#             */
-/*   Updated: 2024/04/20 22:28:20 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/04/21 15:59:19 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-t_stack_node	*db_lstnew(int value)
+t_db_list	*db_lstnew(int value)
 {
-	t_stack_node	*new;
+	t_db_list	*new;
 
-	new = (t_stack_node *)malloc(sizeof(t_list));
+	new = (t_db_list *)malloc(sizeof(t_db_list));
 	if (NULL == new)
 		return (NULL);
 	new->value = value;
 	new->prev = NULL;
 	new->next = NULL;
 	return (new);
+}
+
+t_db_list	*db_lstlast(t_db_list *lst)
+{
+	if (NULL == lst)
+		return (NULL);
+	while (NULL != lst->next)
+	{
+		lst = lst->next;
+	}
+	return (lst);
+}
+
+void	db_lstadd_back(t_db_list **lst, t_db_list *element)
+{
+	t_db_list	*last;
+	t_db_list	*previous;
+
+	if (NULL != lst && NULL != element)
+	{
+		if (NULL != *lst)
+		{
+			last = db_lstlast(*lst);
+			last->next = element;
+			element->prev = last;
+		}
+		else
+		{
+			*lst = element;
+			element->prev = NULL;
+		}
+	}
 }
