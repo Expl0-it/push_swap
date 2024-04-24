085e550bbb55245de946e1111f50dd5fb432ea03
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-04-24 13:40:03 +0200

    refactoring to push, got rid of code repetition

--- src/push.c
+++ src/push.c
@@ -6,30 +6,33 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 10:47:56 by mamichal          #+#    #+#             */
-/*   Updated: 2024/04/24 12:16:07 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/04/24 13:39:23 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int	pa(t_db_list *a, t_db_list *b)
+int	push(t_db_list **src, t_db_list **dst)
 {
 	t_db_list	*tmp;
 
-	if (NULL == b)
+	if (NULL == *src)
 		return (1);
-	tmp = b;
-	b = b->next;
-	if (NULL == a)
+	tmp = *src;
+	*src = (*src)->next;
+	if (*src)
+		(*src)->prev = NULL;
+	tmp->prev = NULL;
+	if (NULL == dst)
 	{
-		a = tmp;
-		a->next = NULL;
+		tmp->next = NULL;
+		*dst = tmp;
 	}
 	else
 	{
-		a->prev = tmp;
-		tmp->next = a;
-		a = tmp;
+		tmp->next = *dst;
+		tmp->next->prev = tmp;
+		*dst = tmp;
 	}
 	return (0);
 }
