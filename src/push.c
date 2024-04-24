5216f688f9b23ba44c95d4e0d1aa5ca691f0f9b2
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-04-24 10:56:25 +0200

    started working on push, to be revised

---
+++ src/push.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 10:47:56 by mamichal          #+#    #+#             */
+/*   Updated: 2024/04/24 10:55:55 by mamichal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+int	pa(t_db_list *a, t_db_list *b)
+{
+	t_db_list *tmp;
+
+	if (NULL == b)
+		return (1);
+	tmp = b;
+	b = b->next;
+	if (NULL == a)
+	{
+		a = tmp;
+		a->next = NULL;
+	}
+	else
+	{
+		a->prev = tmp;
+		tmp->next = a;
+		a = tmp;
+	}
+	return (0);
+}
