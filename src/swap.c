d05938934aa40bbcf2b4acf3a357a44809237c07
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-04-28 18:48:27 +0200

    renamed function swap to static, added ss

--- src/swap.c
+++ src/swap.c
@@ -6,13 +6,13 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 23:20:18 by mamichal          #+#    #+#             */
-/*   Updated: 2024/04/25 20:43:11 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/04/28 18:47:24 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	swap(t_db_list	**stack)
+static void	swap(t_db_list	**stack)
 {
 	if (NULL == *stack || NULL == stack || NULL == (*stack)->next)
 		return ;
@@ -41,4 +41,12 @@ void	sb(t_db_list **b, bool checker)
 	swap(b);
 	if (!checker)
 		ft_printf("sb");
+}
+
+void	ss(t_db_list **a, t_db_list **b, bool checker)
+{
+	swap(a);
+	swap(b);
+	if (!checker)
+		ft_printf("ss");
 }
