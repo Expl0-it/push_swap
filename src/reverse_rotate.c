d5a5508b66f152496517e1c3394fd4a3f3b51a28
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-04-29 13:51:59 +0200

    tested reverse_rotate, added \n when displaying command

--- src/reverse_rotate.c
+++ src/reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 22:20:12 by mamichal          #+#    #+#             */
-/*   Updated: 2024/04/28 18:47:31 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/04/29 13:37:12 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,14 @@ void	rra(t_db_list **a, bool checker)
 {
 	reverse_rotate(a);
 	if (!checker)
-		ft_printf("rra");
+		ft_printf("rra\n");
 }
 
 void	rrb(t_db_list **b, bool checker)
 {
 	reverse_rotate(b);
 	if (!checker)
-		ft_printf("rrb");
+		ft_printf("rrb\n");
 }
 
 void	rrr(t_db_list **a, t_db_list **b, bool checker)
@@ -45,5 +45,5 @@ void	rrr(t_db_list **a, t_db_list **b, bool checker)
 	reverse_rotate(a);
 	reverse_rotate(b);
 	if (!checker)
-		ft_printf("rrr");
+		ft_printf("rrr\n");
 }
