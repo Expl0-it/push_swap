36f8f960ec7bf566e63cc23e30f97a6685e76aa9
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-05-06 07:52:03 +0200

    updated reverse_rotate command to work with s_stack

--- src/reverse_rotate.c
+++ src/reverse_rotate.c
@@ -6,44 +6,44 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 22:20:12 by mamichal          #+#    #+#             */
-/*   Updated: 2024/04/29 13:37:12 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/05/05 21:53:42 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-static void	reverse_rotate(t_db_list **stack)
+static void	reverse_rotate(t_db_list **node)
 {
 	t_db_list	*last_node;
 
-	if (NULL == stack || NULL == *stack || NULL == (*stack)->next)
+	if (NULL == node || NULL == *node || NULL == (*node)->next)
 		return ;
-	last_node = db_lstlast(*stack);
+	last_node = db_lstlast(*node);
 	last_node->prev->next = NULL;
 	last_node->prev = NULL;
-	last_node->next = *stack;
+	last_node->next = *node;
 	last_node->next->prev = last_node;
-	*stack = last_node;
+	*node = last_node;
 }
 
-void	rra(t_db_list **a, bool checker)
+void	rra(t_stack *a, bool checker)
 {
-	reverse_rotate(a);
+	reverse_rotate(&a->node);
 	if (!checker)
 		ft_printf("rra\n");
 }
 
-void	rrb(t_db_list **b, bool checker)
+void	rrb(t_stack *b, bool checker)
 {
-	reverse_rotate(b);
+	reverse_rotate(&b->node);
 	if (!checker)
 		ft_printf("rrb\n");
 }
 
-void	rrr(t_db_list **a, t_db_list **b, bool checker)
+void	rrr(t_stack *a, t_stack *b, bool checker)
 {
-	reverse_rotate(a);
-	reverse_rotate(b);
+	reverse_rotate(&a->node);
+	reverse_rotate(&b->node);
 	if (!checker)
 		ft_printf("rrr\n");
 }
