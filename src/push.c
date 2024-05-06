83f6cd94cece2906d5cd1c471fe95cf03b9ff4c3
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-05-06 07:53:30 +0200

    refactoring end: updated push to work with s_stack, now also takes care of adding or substracting stack sizes

--- src/push.c
+++ src/push.c
@@ -6,48 +6,48 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 10:47:56 by mamichal          #+#    #+#             */
-/*   Updated: 2024/05/05 14:13:34 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/05/05 21:01:42 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-static void	push(t_db_list **dst, t_db_list **src, int *size_dst, int *size_src)
+static void	push(t_stack *dst, t_stack *src)
 {
 	t_db_list	*tmp;
 
-	if (NULL == *src)
+	if (NULL == (*src).node)
 		return ;
-	tmp = *src;
-	*src = (*src)->next;
-	if (*src)
-		(*src)->prev = NULL;
+	tmp = (*src).node;
+	(*src).node = (*src).node->next;
+	if ((*src).node)
+		(*src).node->prev = NULL;
 	tmp->prev = NULL;
-	if (NULL == *dst)
+	if (NULL == (*dst).node)
 	{
 		tmp->next = NULL;
-		*dst = tmp;
+		(*dst).node = tmp;
 	}
 	else
 	{
-		tmp->next = *dst;
+		tmp->next = (*dst).node;
 		tmp->next->prev = tmp;
-		*dst = tmp;
+		(*dst).node = tmp;
 	}
-	*size_src = *size_src - 1;
-	*size_dst = *size_dst + 1;
+	(*src).size = (*src).size - 1;
+	(*dst).size++;
 }
 
-void	pa(t_db_list **a, t_db_list **b, t_stack_sizes *sizes, bool checker)
+void	pa(t_stack *a, t_stack *b, bool checker)
 {
-	push(a, b, &(sizes->size_a), &(sizes->size_b));
+	push(a, b);
 	if (!checker)
 		ft_printf("pa\n");
 }
 
-void	pb(t_db_list **b, t_db_list **a, t_stack_sizes *sizes, bool checker)
+void	pb(t_stack *b, t_stack *a, bool checker)
 {
-	push(b, a, &(sizes->size_b), &(sizes->size_a));
+	push(b, a);
 	if (!checker)
 		ft_printf("pb\n");
 }
