daf434dcbd9864d07691a421481c7e0b7ac36e54
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-05-07 23:26:46 +0200

    renamed is_sorted to stack_is_sorted to be more explicit

--- src/stack_utils.c
+++ src/stack_utils.c
@@ -6,13 +6,13 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 11:24:24 by mamichal          #+#    #+#             */
-/*   Updated: 2024/05/07 21:30:49 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/05/07 22:48:24 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-bool	is_sorted(t_db_list *node)
+bool	stack_is_sorted(t_db_list *node)
 {
 	if (NULL == node)
 		return (true);
