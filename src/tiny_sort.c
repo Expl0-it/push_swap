34e8813eca7d22bcc6923482bdd17a9abb09009c
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-05-19 17:02:44 +0200

    renamed sort_five to a_leave_three

--- src/tiny_sort.c
+++ src/tiny_sort.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 22:50:59 by mamichal          #+#    #+#             */
-/*   Updated: 2024/05/19 16:01:58 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/05/19 16:30:11 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,12 +50,13 @@ void	tiny_sort(t_stack *a)
 		sa(a, false);
 }
 
-void	sort_five(t_stack *a, t_stack *b)
+void	a_leave_three(t_stack *a, t_stack *b)
 {
 	while (a->size > 3)
 	{
 		init_list_utils(a, b);	
 		put_node_on_top(a, find_smallest(a->node), 'a');
 		pb(b, a, false);
+		ft_printf("%d", a->size);
 	}
 }
