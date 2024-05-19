1d42b77d1b3a87ebbeb7145732d21f90ee7894be
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-05-19 17:01:33 +0200

    changed push to be easier to read

--- src/push.c
+++ src/push.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 10:47:56 by mamichal          #+#    #+#             */
-/*   Updated: 2024/05/07 22:48:05 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/05/19 17:01:14 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,26 @@ static void	push(t_stack *dst, t_stack *src)
 {
 	t_db_list	*tmp;
 
-	if (NULL == (*src).node)
+	if (NULL == src->node)
 		return ;
-	tmp = (*src).node;
-	(*src).node = (*src).node->next;
-	if ((*src).node)
-		(*src).node->prev = NULL;
+	tmp = src->node;
+	src->node = src->node->next;
+	if (src->node)
+		src->node->prev = NULL;
 	tmp->prev = NULL;
-	if (NULL == (*dst).node)
+	if (NULL == dst->node)
 	{
 		tmp->next = NULL;
-		(*dst).node = tmp;
+		dst->node = tmp;
 	}
 	else
 	{
-		tmp->next = (*dst).node;
+		tmp->next = dst->node;
 		tmp->next->prev = tmp;
-		(*dst).node = tmp;
+		dst->node = tmp;
 	}
-	(*src).size = (*src).size - 1;
-	(*dst).size = (*dst).size + 1;
+	src->size = (src->size) - 1;
+	dst->size = (dst->size) + 1;
 }
 
 void	pa(t_stack *a, t_stack *b, bool checker)
