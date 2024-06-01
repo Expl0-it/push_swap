8c26aee3585ae0893e0ec55896d1e6377377d0d8
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-06-01 15:03:08 +0200

    changed push_swap func to be much more efficient (from 32k moves to 5.5 avg)

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 22:50:59 by mamichal          #+#    #+#             */
-/*   Updated: 2024/05/25 13:37:44 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/06/01 14:49:26 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,27 @@
 
 void	push_swap(t_stack *a, t_stack *b)
 {
-	if (a->size > 3)
+	t_db_list	*smallest;
+
+	if (a->size == 5)
 		a_leave_three(a, b);
+	else
+	{
+		while (a->size > 3)
+			pb(b, a, false);
+	}
 	tiny_sort(a);
-	while(b->node)
-		pa(a, b, false);
+	while (b->node)
+	{
+		init_list_utils(*a, *b);
+		move_nodes(a, b);
+	}
+	set_position(*a);
+	smallest = find_smallest(a->node);
+	if (smallest->utils.above_median)
+		while (a->node != smallest)
+			ra(a, false);
+	else
+		while (a->node != smallest)
+			rra(a, false);
 }
