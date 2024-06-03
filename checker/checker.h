a8f7f3cd8be0c2adebc3d9402cb5dd17995aa569
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-06-03 14:21:26 +0200

    added checker.h to store various header files

---
+++ checker/checker.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 17:51:07 by mamichal          #+#    #+#             */
+/*   Updated: 2024/06/03 14:21:04 by mamichal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHECKER_H
+# define CHECKER_H
+
+// INCLUDES
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <fcntl.h>
+// TODO: DLEATE LATER AFTER ADDING PRINTF TO LIBFT
+//# include "../libft/includes/ft_printf.h"
+# include "../includes/push_swap.h"
+# include "./get_next_line.h"
+
+#endif
