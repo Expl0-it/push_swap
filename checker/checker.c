fd2e69883664fe576b490c24d5866a316cd07d06
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-06-03 15:04:16 +0200

    started working on main in checker

--- checker/checker.c
+++ checker/checker.c
@@ -10,3 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./checker.h"
+
+int	main(int argc, char **argv)
+{
+	t_stack	a;
+	t_stack	b;
+	char	*next_line;
+
+	a.node = NULL;
+	b.node = NULL;
+	if (argc < 2 || !argv[1][0])
+		return (NOT_ENOUGH_ARGUMENTS);
+	else if (2 == argc)
+		argv = ft_split(argv[1], ' ');
+	stack_init(&a, argv, 2 == argc);
+	stack_set_sizes(&a, &b);
+	next_line = get_next_line(STDIN_FILENO);
+	while (next_line)
+	{
+		exec_command();
+		next_line = get_next_line(STDIN_FILENO);
+	}
+	if (stack_is_sorted(a.node) && b.size == 0)
+		write(STDOUT_FILENO, "OK\n", 3);
+	else
+		write(STDOUT_FILENO, "KO\n", 3);
+	free_stacks(&a, &b);
+	return (OK);
+}
